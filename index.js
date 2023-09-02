e872ad1e93fe650a6670180a5ea9b68347d2b877
Susana Solis Flores <Suzy.flores@icloud.com>
2023-09-02 14:35:45 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random= Math.floor(Math.random() * 10) + 1;
+
+let num3 = 5;
+let num4 = 1;
+let mod = num3 - num4;
+
+let max = 20;
+Math.max(5, 10, 15, 17, 20);
